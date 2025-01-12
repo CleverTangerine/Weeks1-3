97d7c2e3a143c794cd95a03c8cde7d0959073f60
CleverTangerine <99409925+CleverTangerine@users.noreply.github.com>
2025-01-12 17:50:13 -0500

    Finished 2.2 Time and frame rate

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -15,12 +15,14 @@ public class FirstScript : MonoBehaviour
     void Update()
     {
         Vector2 pos = transform.position;
-        pos.x += speed;
+        pos.x += speed * Time.deltaTime;
         Vector2 squareInScreenSpace = Camera.main.WorldToScreenPoint(pos);
 
         if((squareInScreenSpace.x < 0 && speed < 0) || (squareInScreenSpace.x > Screen.width && speed > 0))
         {
             speed *= -1;
+            //Vector3 fixedPos = new Vector3(Screen.width, 0, 0);
+            //pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
         }
 
         transform.position = pos;
